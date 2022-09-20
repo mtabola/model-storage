ffa55f86380e518aa56f03d90576dc784ff5dbee
Mikhail Tabola <m-tabola@mail.ru>
2022-09-20 11:53:19 +0300

    Edit Registration view

--- src/views/auth-view/RegView.vue
+++ src/views/auth-view/RegView.vue
@@ -1,17 +1,22 @@
 <template>
-    <v-form class="authorization">
-        <v-card>
-           <v-card-text>
-              <v-text-field label="Username"/>
-              <v-text-field label="Password" type="password" />
-           </v-card-text>
-           <v-card-actions>
-              <v-btn type="submit">
-                 Login
-              </v-btn>
-           </v-card-actions>
-        </v-card>
-     </v-form>
+   <v-container>
+      <v-form class="authorization" elevating>
+         <v-card>
+            <v-card-text>
+               <v-text-field label="Имя"/>
+               <v-text-field label="Фамилия"/>
+               <v-text-field label="E-mail"/>
+               <v-text-field label="Пароль" type="password"/>
+               <v-text-field label="Повторите пароль" type="password"/>
+            </v-card-text>
+            <v-card-actions>
+               <v-btn type="registration">
+                  Зарегистрироваться
+               </v-btn>
+            </v-card-actions>
+         </v-card>
+      </v-form>
+   </v-container>
   </template>
   
   <script>
